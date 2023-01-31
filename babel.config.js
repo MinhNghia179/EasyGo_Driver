0be719fe0b4749757b0bf8eb0fdd379a745cdf1e
Minh Nghia <nghia.nguyenminh@powergatesoftware.com>
2023-01-31 13:31:20 +0700

    update

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(false);
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: 'react-native-dotenv',
+          verbose: true,
+          path: '.env',
+        },
+      ],
+    ],
+  };
 };
